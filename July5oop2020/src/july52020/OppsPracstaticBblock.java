0883f99f71dd97136abea9509dd855489cd1c3a8
Mahesh <mthakre26@gmail.com>
2020-07-06 17:40:05 +0530

    First try commit and push

---
+++ July5oop2020/src/july52020/OppsPracstaticBblock.java
@@ -0,0 +1,10 @@
+package july52020;
+
+public class OppsPracstaticBblock {
+
+	static{System.out.println("static block is invoked");}  
+	  public static void main(String args[]){  
+	   System.out.println("Hello main");  
+	  }  
+	
+}
